7509306fe7ab28dd980d725794aede689e6fdecb
Franco Arrano Silva <farrano@kdu.cl>
2023-04-03 15:25:57 -0400

    Repara  el footer sobrepuesto

--- Components/footer.js
+++ Components/footer.js
@@ -2,7 +2,7 @@ import adri_logo from '../public/adri.png'
 import Image from 'next/image'
 export default function Footer() {
   return (
-    <div className="fixed z-40 inset-x-0 bottom-0 bg-stone-900  px-2 sm:px-4 py-2.5 rounded">
+    <div className=" bg-stone-900  px-2 sm:px-4 py-2.5 rounded">
       <div className="container flex flex-wrap items-center mx-auto justify-between ">
         <Image src={adri_logo} alt="Adri Gestion Inmobiliaria" className="logo-img-footer" />
         <div className="font-mono fonts text-l  text-neutral-50">
